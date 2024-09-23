72e3acde36c20ddd062b48a91d3cc643be7462d9
Anirban Dutta <meanirbandutta22@gmail.com>
2024-09-24 01:48:34 +0530

    Report Card layout , styles changed

--- frontend/src/components/govt/ReportCard.jsx
+++ frontend/src/components/govt/ReportCard.jsx
@@ -4,20 +4,40 @@ import { GrDownload } from "react-icons/gr";
 import { IoEyeOutline } from "react-icons/io5";
 import { MdDeleteOutline } from "react-icons/md";
 
-const icons = `btn-primary px-2 font-bold text-xl cursor-pointer`;
+const icons = `btn-primary px-2 font-bold text-base cursor-pointer`;
 
-const ReportCard = ({ report_type, startup_type, id, name, date }) => {
+const ReportCard = ({
+  report_heading,
+  report_type,
+  startup_type,
+  id,
+  name,
+  date,
+}) => {
   return (
-    <div className="dashboard-box flex flex-col gap-y-3">
-      <h1 className="text-xl">Lorem ipsum dolor sit amet.</h1>
-      <span className="text-violet-500 font-medium">{report_type}</span>
-      <span>{startup_type}</span>
-      <p className="text-md">
-        Application ID : <span className="font-medium">{id}</span> Startup Name
-        : <span className="font-medium">{name}</span>
-      </p>
+    <div className="dashboard-box w-full flex flex-col justify-between gap-y-2 capitalize">
+      <h1 className="text-xl font-medium h-10 whitespace-nowrap">
+        {report_heading}
+      </h1>
+      <div className="h-[7rem]">
+        <span className="text-violet-500 font-medium underline">
+          {report_type}
+        </span>
+        <span className="block bg-gray-200 px-2 py-1 rounded my-2 w-fit">
+          {startup_type}
+        </span>
+        <p className="text-md uppercase">
+          Application ID : <span className="font-medium">{id}</span>
+        </p>
+        <p className="text-md uppercase">
+          Startup Name : <span className="font-medium">{name}</span>
+        </p>
+      </div>
       <div className="w-full flex items-center gap-x-5 border-t border-gray-200 py-3 cursor-pointer">
-        <span className="flex-1 text-sm">{date}</span>
+        <span className="flex-1 text-sm">
+          <span className="font-medium">Date : </span>
+          {date}
+        </span>
         <button className={icons}>
           <IoEyeOutline />
         </button>
