98ec076e3c91d047c2a4725e85853eb211fb1021
Anirban Dutta <meanirbandutta22@gmail.com>
2024-09-24 01:46:00 +0530

    different colors added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary1: "#4CAF50",
+        primary1Dark: "#2E7D32",
+        primary1Light1: "#81C784",
+        primary1Light2: "#A5D6A7",
+        primary1Light3: "#C8E6C9",
+        primary1Light4: "#E8F5E9",
+        primaryBg1: "#D3D3D3",
+        primaryBg1Light1: "#F5F5F5",
+      },
+    },
   },
   plugins: [],
 };
